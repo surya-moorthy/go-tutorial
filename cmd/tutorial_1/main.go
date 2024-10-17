7d984d51b2a1c964df77909b0928fae3cfe632f8
surya-moorthy <169181413+surya-moorthy@users.noreply.github.com>
2024-10-17 21:03:08 +0530

    first tutorial in go

---
+++ cmd/tutorial_1/main.go
@@ -0,0 +1,27 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("hello world")
+// }
+package main
+
+import "fmt"
+
+func main() {
+	// variable declarations var and const
+	// data types
+	// 1) int , int8 , int16 , int32 , int64
+	// 2) float , float32 , float64
+	// 3) string
+	// 4) bool - true or false
+	var intNum int = 30
+	str1 := "Hello World"
+	const str string = `Hellow owrls 
+	and i am not your man`
+
+	fmt.Println(str)
+	fmt.Println(intNum)
+
+}
---
+++ cmd/tutorial_1/num.py
@@ -0,0 +1,3 @@
+for i in range(0,1000001):
+    print(i)
+  
